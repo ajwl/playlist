881df0773e369e84338ca5979c2dff7b4ef8c290
Anna <ajwleach@gmail.com>
2016-11-17 08:55:17 +0000

    click events more

--- public/js/play.js
+++ public/js/play.js
@@ -2,19 +2,84 @@
 
 var CDAPP = (function(){
 
-    function myFunction(){
+    //Grab src values from html to create Audio objects
+    var audioElements = document.getElementsByTagName('audio'),
+        audioSrcs = [],
+        audioObjects = [],
+        songElements = document.querySelectorAll('.song');
 
-    }
+    console.log("this is" + audioElements);
 
-    return {
+
+    var getSources = function(el){
+        for(var i=0; i<el.length; i++ ){
+            audioSrcs.push(el[i].src);
+        }
+        console.log(audioSrcs);
+        createAudioObjects(audioSrcs);
+    };
+
+    var createAudioObjects = function(sr){
+        for(var i=0; i<sr.length; i++){
+            audioObjects.push(new Audio(sr[i]));
+        }
+        console.log(audioObjects);
+        return audioObjects
+    };
+
+    var attachEvent = function(songBox){
+
+        for(var i=0; i<songBox.length; i++){
+
+            songBox[i].addEventListener(
+                "click",
+                assignAction,
+                false
+            );
+        };
+    };
+
+    var assignAction = function (e){
+
+        console.log("here, e is this " + e.target);
+
+        //Find audio file associated with event target
+
+        var clickedElement = document.getElementById(e.target.id);
+
+        console.log(clickedElement);
+
+        var audioToPlay = clickedElement.getElementsByTagName('audio');
+
+        console.log(audioToPlay);
+
+        if(audioToPlay.paused === true){
+            audioToPlay.play();
+        }
+        else{
+            audioToPlay.pause();
+        }
 
     };
 
+
+    getSources(audioElements);
+    attachEvent(songElements);
+
+
+    //
+    // if(audios.length === imgs.length){
+    //     attachPlayEvent();
+    // }
+    // else {
+    //     //Append an error message
+    // }
+
+
 })();
 
-CDAPP.myfunction();
+//CDAPP.init();
 
-//https://webdesign.tutsplus.com/tutorials/create-a-customized-html5-audio-player--webdesign-7081
 
 
 
